44ae735eb23612a2a80d4fb642c0d483910704d3
Chantz-J <johnson.chantz@gmail.com>
2021-02-03 00:55:24 -0500

    Added createPage export function and returned result to dynamically create URL and pages.

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,6 +13,33 @@ module.exports.onCreateNode = ({node, actions}) => {
             name: 'slug',
             value: slug
         })
-    }
-    
+    }  
+}
+
+module.exports.createPages = async ({graphql , actions}) => {
+    const { createPage } =  actions
+    const blogPostTemplate = path.resolve(`./src/templates/blog.js`)
+    const result = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                  node {
+                    fields {
+                      slug
+                    }
+                  }
+                }
+            }
+        }
+    `)
+    result.data.allMarkdownRemark.edges.forEach(x => {
+        createPage({
+            path: `/blog/${x.node.fields.slug}`, //The URL
+            component: blogPostTemplate,
+            context: {
+                slug: x.node.fields.slug
+            }
+        })
+    })
+
 }
